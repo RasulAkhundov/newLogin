00f98980a2446ac12f278207850ef4b775ad73ee
Resul <resul.axundov2002@mail.ru>
2019-11-18 16:55:14 +0400

    slama

--- index.js
+++ index.js
@@ -12,21 +12,22 @@ app.use(require("express").static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+console.log("salam");
 
 // router init
 app.use("/api", authRouter);
 
 // tapilmayan routerler uchun
 app.get("/", (req, res) => {
-   res.render("login")
+  res.render("login");
 });
 
 app.get("/register", (req, res) => {
-  res.render("register")
+  res.render("register");
 });
 
 app.get("/news", (req, res) => {
-  res.render("news")
+  res.render("news");
 });
 
 // app.get("/*", (req, res) => {
@@ -34,13 +35,17 @@ app.get("/news", (req, res) => {
 // });
 
 // mongoose connect
-mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://localhost/breaking-news', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, () => console.log("mongodb is ready"));
+mongoose.set("useCreateIndex", true);
+mongoose.connect(
+  "mongodb://localhost/breaking-news",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => console.log("mongodb is ready")
+);
 
 // server
 app.listen(4004, () => {
-    console.log("server started in 4004 port")
+  console.log("server started in 4004 port");
 });
